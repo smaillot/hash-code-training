4d6cf56921c4c19b99d21ce45aa533b4f0ea493f
Seboran <nirinarabeson@outlook.fr>
2018-02-24 22:48:33 +0100

    pizza is now a np array with booleans and disp_pizza function

--- pizza/IO.py
+++ pizza/IO.py
@@ -1,4 +1,6 @@
 from verbose import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 def write_list(f, list):
 
@@ -11,19 +13,31 @@ def write_array(f, array):
         write_list(f, line)
 
 def read_input(reader):
-    
-    ## init params
+    '''
+    init params
     # number of rows, number of columns, minimum number of each ingredient cells in a slice, maximum number of cells of a slice
+    True is a mushroom, False is a Tomato
+    '''
     R, C, L, H = [int(i) for i in reader.readline().split(" ")]
     
     
-    pizza = []
-    for _ in range(R):
+    pizza = np.zeros([R, C], dtype = bool)
+    for i in range(R):
         
-        pizza.append(list(reader.readline().rstrip()))
+        pizza[i, :] = [c == 'M' for c in list(reader.readline().rstrip())]
     
     
     return R, C, L, H, pizza
+
+def disp_pizza(pizza):
+    '''
+    Violet is a tomato
+    Yellow is a mushroom
+    '''
+
+    plt.imshow(pizza)
+    plt.show()
+
 
 def write_output(f, list_output, array_output):
 
