14d6c49e14e1c0c91386ddd7a7bba3dd816285a9
Seboran <nirinarabeson@outlook.fr>
2018-02-25 00:21:04 +0100

    edited some bugs in IO.py and added a generator of all slices

--- pizza/solution.py
+++ pizza/solution.py
@@ -4,3 +4,24 @@ from __future__ import print_function
 
 # custom
 from verbose import *
+import numpy as np
+
+def generate_all_slices(R, C, L, H):
+    '''
+    L : minimum number of each ingredient in a slice
+    H : max size for a slice
+    We generate all the possible slices that we can make out of the pizza. We know that their size (or area) is between 2 * L and H
+    The generated slices start at 0, 0
+    '''
+    list_of_slices = []
+    for A in range(2 * L, H + 1):
+        
+        for row_size in range(1, A + 1):
+            
+            if A % row_size == 0:
+                column_size = A // row_size
+                if column_size < (C + 1):
+                    list_of_slices.append([0, 0, row_size - 1, column_size - 1])
+                
+    return list_of_slices
+
