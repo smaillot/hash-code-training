78ab89de4e8ee17c49b6c6a6dd17b4c806e62c10
Seboran <nirinarabeson@outlook.fr>
2018-02-25 13:17:24 +0100

    changed number of cores

--- pizza/main.py
+++ pizza/main.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     ###########################
     
     pizza_test = Pizza(R, C, L, H, pizza)
-    pizza_tests = [Pizza_seed(pizza_test, i) for i in range(4)]
+    pizza_tests = [Pizza_seed(pizza_test, i) for i in range(56)]
     best_score = mp.Value('i', 0)
     slices = mp.Manager().list()
     lock = mp.Lock()
