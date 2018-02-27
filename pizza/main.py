c59e4f5e1d1dfd71e93bf2c5d5e3d09203740773
Stéphane Maillot <s.maillot2@gmail.com>
2018-02-27 09:26:02 +0100

    progress saving

--- pizza/main.py
+++ pizza/main.py
@@ -22,7 +22,7 @@ parser.add_argument('input', help='path to input file', type=argparse.FileType("
 parser.add_argument('output', help='path to outputfile', type=argparse.FileType("wt"))
 parser.add_argument('-i', help='height of subblocs', type=int)
 parser.add_argument('-j', help='width of subblocs', type=int)
-parser.add_argument('-s', help='score needed', type=int)
+parser.add_argument('-s', help='desired score', type=int)
 parser.add_argument('-b', help='batch size', type=int)
 
 args = parser.parse_args()
@@ -39,44 +39,53 @@ pizzas = sub_pizzas(pizza, R, C)
 ## DO (GOOD) STUFF HERE
 ###########################
 
-final_slices = []
-n=1
-    
-for p, i, j in pizzas:
-    
-    R = p.shape[0]
-    C = p.shape[1]
-    lim = R * C * args.s / R_tot / C_tot
-    best = 0
+area = R_tot * C_tot
+precision = np.floor(area / 2)
 
-    while best < lim:
+while precision < args.s:
+    
+    final_slices = []
+    n=1
         
-        slices = generate_best_solution(R, C, L, H, p, args.b)
-        score = compute_score(slices)
-        if score > best:
-            best = score
+    for p, i, j in pizzas:
+        
+        R = p.shape[0]
+        C = p.shape[1]
+        lim = R * C * precision / area
+        best = 0
+    
+        while best < lim:
             
-        try:
-            print("slice " + str(n) + " / " + str(len(pizzas)) + "\tbest\t" + str(best) + "\t( " + str(best / R / C * 100) + "% )\t/ " + str(lim) + "\t( " + str(lim / R / C * 100) + "% )")
-        except:
-            pass
-        
-    slices_transl = [[sl[0]+i, sl[1]+j, sl[2]+i, sl[3]+j] for sl in slices]
-    final_slices += slices_transl
-    n+=1
+            slices = generate_best_solution(R, C, L, H, p, args.b)
+            score = compute_score(slices)
+            if score > best:
+                best = score
+                
+            try:
+                print("slice " + str(n) + " / " + str(len(pizzas)) + "\tbest\t" + str(best) + "( " + str(best / R / C * 100) + "% )\t/ " + str(lim) + "( " + str(lim / R / C * 100) + "% )")
+            except:
+                pass
+            
+        slices_transl = [[sl[0]+i, sl[1]+j, sl[2]+i, sl[3]+j] for sl in slices]
+        final_slices += slices_transl
+        n+=1
+    
+    precision = np.floor((3 * precision + area) / 4)
+
+    score_before = compute_score(final_slices)
+    final_slices = extend_slices(final_slices, pizza, R_tot, C_tot, L, H)
+    valid = check_slices(final_slices, pizza, R_tot, C_tot, L, H)
+    score = compute_score(final_slices) * valid
+    args.output.seek(0) 
+    args.output.truncate()       
+    write_output(args.output, final_slices)
+    try:
+        print("Score :\t" + str(score_before) + "\t( " + str(score_before / R_tot / C_tot * 100) + "% )")
+        print("Final score :\t" + str(score) + "\t( " + str(score / R_tot / C_tot * 100) + "% )")
+    except:
+        pass
       
 ###########################
 ## END OF STUFF
 ###########################
 
-score_before = compute_score(final_slices)
-final_slices = extend_slices(final_slices, pizza, R_tot, C_tot, L, H)
-valid = check_slices(final_slices, pizza, R_tot, C_tot, L, H)
-score = compute_score(final_slices) * valid
-write_output(args.output, final_slices)
-
-try:
-    print("Score :\t" + str(score_before) + "\t( " + str(score_before / R_tot / C_tot * 100) + "% )")
-    print("Final score :\t" + str(score) + "\t( " + str(score / R_tot / C_tot * 100) + "% )")
-except:
-    pass
